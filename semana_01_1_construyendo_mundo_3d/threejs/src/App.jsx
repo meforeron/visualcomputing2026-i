1dd2fc05cdef48fd82ffd46d3b6e79bd0edb55fa
Melissa Forero <meforeron@unal.edu.co>
2026-02-11 17:51:25 -0500

    Add basic 3D scene with Three.js

--- semana_01_1_construyendo_mundo_3d/threejs/src/App.jsx
+++ semana_01_1_construyendo_mundo_3d/threejs/src/App.jsx
@@ -1,51 +1,49 @@
+import * as THREE from 'three'
 import { Canvas, useLoader } from '@react-three/fiber'
-import { OrbitControls, Edges } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 
-function Model() {
-  const object = useLoader(OBJLoader, '/model.obj')
-  const points = []
-
-  object.traverse((child) => {
-    if (child.isMesh) {
-      child.material.color.set('orange')
-
-      // vertices
-      points.push(
-        <points key={child.uuid + '-points'} geometry={child.geometry}>
-          <pointsMaterial
-            color="black"
-            size={7}
-            sizeAttenuation={false}
-          />
-        </points>
-      )
-
-      // edges
-      points.push(
-        <Edges key={child.uuid + '-edges'} geometry={child.geometry} color="black" />
-      )
-    }
-  })
+function Cono() {
+  const obj = useLoader(OBJLoader, '/1.obj')
 
   return (
-    <>
-      <primitive object={object} scale={1} />
-      {points}
-    </>
+    <group scale={0.5}>
+      {obj.children.map((child, i) => {
+        if (!child.isMesh) return null
+
+        
+        const edges = new THREE.EdgesGeometry(child.geometry)
+
+        return (
+          <group key={i}>
+            {/* Caras */}
+            <mesh geometry={child.geometry}>
+              <meshStandardMaterial color="orange" />
+            </mesh>
+
+            {/* Aristas */}
+            <lineSegments geometry={edges}>
+              <lineBasicMaterial color="black" linewidth={2} />
+            </lineSegments>
+
+            {/* Vértices */}
+            <points geometry={child.geometry}>
+              <pointsMaterial size={0.09} color="red" />
+            </points>
+          </group>
+        )
+      })}
+    </group>
   )
 }
 
+
 export default function App() {
   return (
-    <Canvas
-      style={{ width: '100vw', height: '100vh' }}
-      camera={{ position: [10, 5, 5], fov: 50 }}
-    >
-      <ambientLight intensity={0.6} />
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      <ambientLight intensity={1} />
       <directionalLight position={[5, 5, 5]} />
-
-      <Model />
+      <Cono />
       <OrbitControls />
     </Canvas>
   )
